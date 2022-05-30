e9bd665d6189299f98871947cb12e0cf74e49a08
Renan Lima <renan.limagomes@gmail.com>
2022-05-30 12:03:13 -0300

    Incluir README e correções de responsividade, script e html

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# Project 4: Around The U.S.
+## EUA Afora
 
-### Overview
+📝Este é um projeto que mostra as lugares explorados pelo EUA.
+Pode ser visualizado em diversos tamanhos de tela populares (1280px, 768px, 320px).
+Foi desenvolvido para a conclusão da Sprint 4 do curso de desenvolvedor web da [Practicum](https://practicum.yandex.com/profile/web/).
 
-- Figma
-- Images
+- [Link para o projeto no Figma](https://www.figma.com/file/XfB6BSINvliub43JgKza1e/WEB.-Sprint-4.-Around-The-U.S.-desktop-%2B-mobile-pt?node-id=0%3A1)
 
-**Figma**
+- [Link para o projeto no Github Pages](https://renanl23.github.io/web_project_4/)
 
-- [Link to the project in Figma](https://www.figma.com/file/XfB6BSINvliub43JgKza1e/WEB.-Sprint-4.-Around-The-U.S.-desktop-%2B-mobile-pt?node-id=0%3A1)
+### Tecnologias usadas
 
-**Images**
+HTML, CSS e Javascript aplicados a [Metodologia BEM](https://en.bem.info/).
 
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster.
-
-Have fun with JavaScript!
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![Javascript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,15 @@
 // Formulário
-const form = document.querySelector(".modal__content");
+const formElement = document.querySelector(".modal__content");
 
 // Entradas
-const titleInput = form.querySelector(".form__title");
-const subtitleInput = form.querySelector(".form__subtitle");
+const titleInput = formElement.querySelector(".form__title");
+const subtitleInput = formElement.querySelector(".form__subtitle");
 
 // Botões de ação
 const profileEditButton = document.querySelector(".profile__edit");
 const closeButton = document.querySelector(".modal__close");
 const saveButton = profileEditButton.querySelector(".modal__button");
+const likeButtons = document.querySelectorAll(".element__like");
 
 // Valores
 const titleValue = document.querySelector(".profile__title");
@@ -16,17 +17,28 @@ const subtitleValue = document.querySelector(".profile__subtitle");
 
 const modal = document.querySelector(".modal");
 
-// Arrow function para escutar o evento de click no botão de fechar e editar
-const modalOpened = () => modal.classList.toggle("modal_opened");
+// Função para abertura e fechamento do modal
+function modalOpened() {
+  return modal.classList.toggle("modal_opened");
+}
+
+// Função para escutar o evento de click no botão de element__like
+likeButtons.forEach((likeButton) => {
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("element__like_clicked");
+  });
+});
 
 profileEditButton.addEventListener("click", modalOpened);
 closeButton.addEventListener("click", modalOpened);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); // Evita o comportamento padrão do formulário
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); // Evita o comportamento padrão do formulário
 
   titleValue.textContent = titleInput.value;
   subtitleValue.textContent = subtitleInput.value;
 
   modalOpened();
-});
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
