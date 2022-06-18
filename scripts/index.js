5743b1e5f387a468ee17b07f94f66e43ff968093
Renan Lima <renan.limagomes@gmail.com>
2022-06-18 18:21:20 -0300

    Concluir parte 1 - Seis Cartões Iniciais

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,53 @@
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
 // Formulário
 const formElement = document.querySelector(".modal__content");
 
 // Entradas
 const titleInput = formElement.querySelector("#title");
 const subtitleInput = formElement.querySelector("#subtitle");
+
+// Template Elements
+const elementsList = document.querySelector(".elements");
+
+function renderElement(card) {
+  const templateElements = document.querySelector("template").content;
+  const element = templateElements.querySelector(".element").cloneNode(true);
+  element.querySelector(".element__image").src = card.link;
+  element.querySelector(".element__image").alt = card.name;
+  element.querySelector(".element__title").textContent = card.name;
+  elementsList.append(element);
+}
+
+function updateElements(cards) {
+  elementsList.innerHTML = "";
+  cards.forEach((card) => renderElement(card));
+}
 
 // Botões de ação
 const profileEditButton = document.querySelector(".profile__edit");
@@ -42,3 +86,4 @@ function handleProfileFormSubmit(evt) {
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+updateElements(initialCards);
