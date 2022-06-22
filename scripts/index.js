56b3804c99e5c084b640c32fdc21cdb23f63499e
Renan Lima <renan.limagomes@gmail.com>
2022-06-22 11:39:32 -0300

    Concluir alterações solicitadas na revisão

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: "The Creel house",
-    link: "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQTP72varvV4PboGihUE_jxkUwa8Zj3Bhcqo65aLTBZ2LUJlyFaMT8ZTqT_YRjWN3ua_wP1Vf8MeSApw3d9bcB9rDcVGQCrdWg4Sjld-Bjva-KtzxzRNh2JXUU1f7gFGDAz6hVtop15A1jTKj_l-XXzh0.jpg?r=af0",
+    name: "Vale de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
   {
-    name: "The lab",
-    link: "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQdJ67LVS71LGIvuHeheKoUR3b4TnqUmDRbdFJKJMcS0UJJ6y9pRojMvBkmWAyuCwyrXf-vkBOTa5IviC8FAzDAMMx6DNKzO5BOeiDO1cF1ml1vtvtN5oo9eSKw6OFa8YRyDNhxv0EIZWp7RquP7jWvPU.jpg?r=625",
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   },
   {
-    name: "Billy’s grave",
-    link: "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQTvbsCC8rNO7IY8frYdAGzBJCQDSHv69Jc1aXrwJgMjNGvqcbey96b5UQx-eBT1MqTcJtXoWYAsdOdDoKhyBECWb8_rnS3bPDua3FM-UUVlF4AVubSGNUrL7Ngyl33fFhtdm8gaIZtiejtAPPboMlgY8.jpg?r=528",
+    name: "Montanhas Carecas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   },
   {
-    name: "Across the Bearing Sea from Alaska",
-    link: "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQVyCW5Egj07H01E9vplpl9Me6ZBH_ohhYmPSIOdftKS5fpYYxQK88nYqX2rdF43bZhT3dQ1rSrSaYVTD3QfgNtwgbOkxOLPD2Y9u0fuzWNIwIMrn8du-MSsv1Z0FEDFNTssde6CMqQ9MAifd7Ujfd0lZ.jpg?r=5cb",
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
   },
   {
-    name: "Upside Down",
-    link: "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQRKm_e0rxNhOOsnADyapTk9q4EBUHGf9MEoBRL0y2G-d-1nDmVQI9uhN_Ozwmoixg_piu6_eWmLyZBJwVhq3Llt6Yo7LJTnVOjO7Kmz67p38DKVX1t36Kd3hPuIBIZP8HZAp74y9CLigpRWrTyLlqLEM.jpg?r=e22",
+    name: "Parque Nacional da Vanoise ",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   },
   {
-    name: "Linha 4 - Amarela",
-    link: "https://vademetrosp.files.wordpress.com/2022/06/stranger-things-na-estacao.jpg?w=1920&h=768&crop=1",
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
 
@@ -49,9 +49,9 @@ function renderElement(card) {
 function updateElements(cards) {
   elementsList.innerHTML = "";
   cards.forEach((card) => renderElement(card));
-  renderLikeButtons();
-  renderTrashButtons();
-  renderFigButtons();
+  setEventLikeButtons();
+  setEventTrashButtons();
+  setEventFigButtons();
 }
 
 // Botões de ação
@@ -68,7 +68,7 @@ const subtitleValue = document.querySelector(".profile__subtitle");
 const modal = document.querySelector(".modal");
 
 // Função para abertura do modal
-function modalOpened(opened) {
+function handleModal(opened) {
   modal.classList.toggle("modal_opened");
   if (opened == "modal__content") {
     formElement.classList.toggle("modal__content_opened");
@@ -80,8 +80,8 @@ function modalOpened(opened) {
 // Elementos do Modal
 const modalTitle = formElement.querySelector(".modal__title");
 
-// Função para renderizar Modal Add Local
-function renderModalAddLocation() {
+// Função para configurar os atributos de Modal Add Local
+function setModalAddLocation() {
   modalTitle.textContent = "Novo Local";
   titleInput.value = "";
   subtitleInput.value = "";
@@ -89,10 +89,15 @@ function renderModalAddLocation() {
   subtitleInput.placeholder = "Link de Imagem";
   saveButton.textContent = "Criar";
   formElement.setAttribute("name", "form__add-local");
-  modalOpened("modal__content");
 }
-// Funcão para renderizar Modal Profile Edit
-function renderModalProfileEdit() {
+
+// Função para renderizar Modal Add Local
+function renderModalAddLocation() {
+  setModalAddLocation();
+  handleModal("modal__content");
+}
+// Função para configurar os atributos de Modal Profile Edit
+function setModalProfileEdit() {
   modalTitle.textContent = "Editar Perfil";
   titleInput.value = titleValue.textContent;
   subtitleInput.value = subtitleValue.textContent;
@@ -100,34 +105,44 @@ function renderModalProfileEdit() {
   subtitleInput.placeholder = "Sobre mim";
   saveButton.textContent = "Salvar";
   formElement.setAttribute("name", "form__edit-profile");
-  modalOpened("modal__content");
 }
-function renderFigButtons() {
+
+// Funcão para renderizar Modal Profile Edit
+function renderModalProfileEdit() {
+  setModalProfileEdit();
+  handleModal("modal__content");
+}
+
+function handleFigButtonEvent(evt) {
+  const figureSource = evt.target;
+  const figure = document.querySelector(".modal__image");
+  const figureCaption = document.querySelector(".modal__figcaption");
+  figure.src = figureSource.src;
+  figure.alt = figureSource.alt;
+  figureCaption.textContent = figureSource.alt;
+  handleModal("modal__fig");
+}
+
+function setEventFigButtons() {
   const openFigButtons = document.querySelectorAll(".element__image");
-  const openedFigButtonEvent = (evt) => {
-    const figureSource = evt.target;
-    const figureRender = document.querySelector(".modal__image");
-    const figureCaption = document.querySelector(".modal__figcaption");
-    figureRender.src = figureSource.src;
-    figureRender.alt = figureSource.alt;
-    figureCaption.textContent = figureSource.alt;
-    modalOpened("modal__fig");
-  };
   openFigButtons.forEach((figButton) => {
-    figButton.addEventListener("click", openedFigButtonEvent);
+    figButton.addEventListener("click", handleFigButtonEvent);
   });
 }
+
+function handleLikeButtonsEvent(evt) {
+  evt.target.classList.toggle("element__like_clicked");
+}
+
 // Função para escutar o evento de click no botão de element__like
-function renderLikeButtons() {
+function setEventLikeButtons() {
   const likeButtons = document.querySelectorAll(".element__like");
-  const likedButtonEvent = (evt) =>
-    evt.target.classList.toggle("element__like_clicked");
   likeButtons.forEach((likeButton) => {
-    likeButton.addEventListener("click", likedButtonEvent);
+    likeButton.addEventListener("click", handleLikeButtonsEvent);
   });
 }
 // Função para escutar o evento de click no botão de element__trash
-function renderTrashButtons() {
+function setEventTrashButtons() {
   const trashButtons = document.querySelectorAll(".element__trash");
   trashButtons.forEach((trashButton) => {
     trashButton.addEventListener("click", removeCard);
@@ -148,13 +163,13 @@ function removeCard(evt) {
 
 profileEditButton.addEventListener("click", renderModalProfileEdit);
 profileAddLocation.addEventListener("click", renderModalAddLocation);
-closeButton.addEventListener("click", () => modalOpened("modal__content"));
-closeFigButton.addEventListener("click", () => modalOpened("modal__fig"));
+closeButton.addEventListener("click", () => handleModal("modal__content"));
+closeFigButton.addEventListener("click", () => handleModal("modal__fig"));
 
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Evita o comportamento padrão do formulário
-  const isProfileEditForm = evt.target.name.includes("form__edit-profile");
-  if (isProfileEditForm) {
+  const profileEditForm = evt.target.name.includes("form__edit-profile");
+  if (profileEditForm) {
     titleValue.textContent = titleInput.value;
     subtitleValue.textContent = subtitleInput.value;
   } else {
@@ -164,7 +179,7 @@ function handleFormSubmit(evt) {
     });
     updateElements(initialCards);
   }
-  modalOpened("modal__content");
+  handleModal("modal__content");
 }
 
 // Adicionar o evento de submit ao formulário do Modal
