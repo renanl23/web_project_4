c91c590817d341d1c4f8aac27d7964685a6bc716
Renan Lima <renan.limagomes@gmail.com>
2022-07-26 20:30:05 -0300

    Incluído validação de formulário

--- scripts/index.js
+++ scripts/index.js
@@ -165,6 +165,80 @@ function removeCard(evt) {
   updateElements(initialCards);
 }
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("modal__input-text_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("modal__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("modal__input-text_error");
+  errorElement.classList.remove("modal__input-error_active");
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  console.log(hasInvalidInput(inputList));
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("modal__button_inactive");
+  } else {
+    buttonElement.classList.remove("modal__button_inactive");
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(".modal__input-text")
+  );
+  const buttonElement = formElement.querySelector(".modal__button");
+
+  // Aqui, para verificar o estado do botão no início:
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      // E aqui, para verificar sempre que qualquer entrada de campo for alterada:
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__content"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    const fieldsetList = Array.from(
+      formElement.querySelectorAll(".modal__set")
+    );
+
+    fieldsetList.forEach((fieldset) => {
+      setEventListeners(fieldset);
+    });
+  });
+};
+
+enableValidation();
+
 profileEditButton.addEventListener("click", renderModalProfileEdit);
 profileAddLocation.addEventListener("click", renderModalAddLocation);
 closeButton.addEventListener("click", handleModalContent);
